56d6b31e159a811142c455472fff7455de537cf4
jpfaw <isidoyuuta@gmail.com>
2017-05-13 19:12:38 +0900

    [add] playingVC

--- spajam2017/PlayingViewController.swift
+++ spajam2017/PlayingViewController.swift
@@ -1,51 +1,54 @@
 //
-//  StartViewController.swift
-//  spajam2017
+//  PlayingViewController.swift
+//  
 //
 //  Created by Yuta on 2017/05/13.
-//  Copyright © 2017年 Yuta. All rights reserved.
+//
 //
 
 import UIKit
 import MapKit
-import CoreLocation
+import RealmSwift
 
-class StartViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
+class Location: Object {
+    dynamic var latitude: Double = 0.0
+    dynamic var longitude: Double = 0.0
+    dynamic var createdAt = Date(timeIntervalSince1970: 1)
+}
 
+class PlayingViewController: UIViewController, CLLocationManagerDelegate {
+  
     @IBOutlet weak var mapView: MKMapView!
-    @IBOutlet weak var dateLabel: UILabel!
-    @IBOutlet weak var timeLabel: UILabel!
+    @IBOutlet weak var startTimeLabel: UILabel!
+    @IBOutlet weak var elapsedTimeLabel: UILabel!
     
-    var coordinate: CLLocationCoordinate2D!
     var locationManager: CLLocationManager!
-
+    
+    let userDefaults = UserDefaults.standard
+    
+    var locations: Results<Location>!
+    var token: NotificationToken!
+    var isUpdating = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         locationManager = CLLocationManager() // インスタンスの生成
         locationManager.delegate = self
         locationManager.distanceFilter = 100.0
         locationManager.desiredAccuracy = kCLLocationAccuracyHundredMeters
-        let status = CLLocationManager.authorizationStatus()
-        
-        // まだ認証が得られていない場合は、認証ダイアログを表示.
-        if(status != CLAuthorizationStatus.authorizedWhenInUse) {
-            locationManager.requestWhenInUseAuthorization()
-        }
-        
+
         // 位置情報の更新を開始.
         locationManager.startUpdatingLocation()
         
         //現在位置を中心にセット
-        mapView.userTrackingMode = MKUserTrackingMode.follow
         mapView.setCenter(mapView.userLocation.coordinate, animated: true)
-        
+        mapView.userTrackingMode = MKUserTrackingMode.follow
 
-        
     }
     
     // GPSから値を取得した際に呼び出されるメソッド.
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-        
+
         // 配列から現在座標を取得.
         let myLocations: NSArray = locations as NSArray
         let myLastLocation: CLLocation = myLocations.lastObject as! CLLocation
@@ -68,29 +71,14 @@ class StartViewController: UIViewController, MKMapViewDelegate, CLLocationManage
     func mapView(_ mapView: MKMapView, regionDidChangeAnimated animated: Bool) {
         print("regionDidChangeAnimated")
     }
-    
-    // 認証が変更された時に呼び出されるメソッド.
-    private func locationManager(manager: CLLocationManager, didChangeAuthorizationStatus status: CLAuthorizationStatus) {
-        switch status{
-        case .authorizedWhenInUse:
-            print("AuthorizedWhenInUse")
-        case .authorized:
-            print("Authorized")
-        case .denied:
-            print("Denied")
-        case .restricted:
-            print("Restricted")
-        case .notDetermined:
-            print("NotDetermined")
-        }
-    } 
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-     /*
+
+    /*
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
